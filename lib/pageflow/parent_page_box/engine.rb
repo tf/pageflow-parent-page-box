9b80726a162a053b2457939c12b4e3e71b24a651
Tim Fischbach <tfischbach@codevise.de>
2016-04-25 23:26:03 +0200

    Add locales

--- lib/pageflow/parent_page_box/engine.rb
+++ lib/pageflow/parent_page_box/engine.rb
@@ -4,6 +4,7 @@ module Pageflow
       isolate_namespace Pageflow::ParentPageBox
 
       config.autoload_paths << File.join(config.root, 'lib')
+      config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.yml').to_s]
     end
   end
 end
